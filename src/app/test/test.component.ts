a79369fba000f4223ced57ee6cc995268b1083af
angga1992 <anggamulya.ghazy@gmail.com>
2018-10-08 19:08:48 +0700

    ngfor

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -18,7 +18,26 @@ import { Component, OnInit } from '@angular/core';
             <div *ngSwitchCase="'red'">picked red</div>
             <div *ngSwitchCase="'yellow'">picked yellow</div>
             <div *ngSwitchCase="'blue'">picked blue</div>
-            <div *ngSwitchCase="'brown'">picked blue</div>
+            <div *ngSwitchDefault>picked again</div>
+          </div>
+          <h2>this ngfor</h2>
+          <div *ngFor="let color of colors; index as i">
+            <h3> {{i}} {{color}} </h3>
+          </div>
+
+          <h2>this ngfor first index</h2>
+          <div *ngFor="let color of colors; first as f">
+            <h3> {{f}} {{color}} </h3>
+          </div>
+
+          <h2>this ngfor last index</h2>
+          <div *ngFor="let color of colors; last as l">
+            <h3> {{l}} {{color}} </h3>
+          </div>
+
+          <h2>this ngfor even index</h2>
+          <div *ngFor="let color of colors; even as e">
+            <h3> {{e}} {{color}} </h3>
           </div>
 
 
@@ -34,7 +53,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  public color = "blue";
+  // public color = "black";
+  public colors = ["red","blue","green", "yellow"]
   public displayName = true;
   public name = "AMS TEAM";
   public user = "";
